c9e58bce6403e8bd3356de6c2ccef524e5d02c13
Jeff_wu_hub <15809812381@163.com>
2020-10-06 21:40:07 +0800

    初始化项目目录

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app">
-    
+    <!-- 页面占位富符号 -->
+    <router-view></router-view>
   </div>
 </template>
 
---
+++ src/components/login.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        测试页面!
+    </div>
+</template>
+<style scoped>
+
+</style>
+<script>
+export default {
+    
+}
+</script>
