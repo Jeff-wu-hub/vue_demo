8389e5cdea7b10e56dc798ee753635f6e831b10e
Jeff_wu_hub <15809812381@163.com>
2020-10-07 14:43:10 +0800

    登陆页面完成

--- src/components/login.vue
+++ src/components/login.vue
@@ -1,16 +1,20 @@
 <template>
-  <div>
+  <div class="bg">
     <div class="main">
-      <div class="photo"><img src="#" alt="头像图片"></div>
+      <div class="photo"><img src="../assets/pexels-photo-179114.jpeg" alt="头像图片"></div>
+      <div class="text"><h2>欢迎登陆XX管理系统</h2></div>
+      <!-- 登陆表单 -->
       <div class="form">
-        <el-input class="input" v-model="user"></el-input>
-        <el-input class="input" v-model="passwd" type="password"></el-input>
-        <el-button class="button" @click="login">Login</el-button>
+        <el-input class="input" v-model="user" prefix-icon="el-icon-user" placeholder="请输入账号" ></el-input>
+        <el-input class="input" v-model="passwd" placeholder="请输入密码" prefix-icon="el-icon-key
+" @keyup.enter.native="login" type="password"></el-input>
+        <el-button class="button" @click="login" >Login</el-button>
       </div>
     </div>
   </div>
 </template>
 <style lang="css" scoped>
+/* 整体form */
 .main{
   box-shadow: #000010 10px 10px 50px;
   width: 550px;
@@ -21,12 +25,35 @@
   transform: translate(-50%,-50%);
   border-radius: 20px 20px;
 }
+/* 头像 */
   .photo{
-  background-color:#ff11ff;
+  position: relative;
+  width: 100px;
+  height: 100px;
+  border-radius: 50%;
+  overflow: hidden;
+  top: -50px;
+  left: 50%;
+  transform: translate(-50%);
+  border-top:solid 0.5px;
+  box-sizing: border-box;
+
   }
+  .photo img{
+    width: 100%;
+    height: 100%;
+  }
+  /* 文本h2标题 */
+  .text{
+    position: absolute;
+    top: 20%;
+    left: 50%;
+    transform: translate(-50%);
+  }
+  /* 输入表单 */
   .form{
     position: relative;
-    top: 20%;
+    top: 5%;
     left:50%;
     transform: translate(-50%);
     width: 60%;
@@ -45,12 +72,15 @@
 export default {
   data () {
     return {
+    // 账号密码
       user: '',
       passwd: ''
     }
   },
   methods: {
+    // 登陆方法
     login () {
+      alert(this.user)
     }
   }
 }
