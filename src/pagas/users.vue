5959a91bd1485f1a5d8c6a755e91a774ed84fa47
Jeff-wu-hub <15809812381@163.com>
2020-11-09 23:55:37 +0800

    ！！完场修改用户信息操作 ！！！！删除操作有bug出现
    Signed-off-by: Jeff-wu-hub <15809812381@163.com>

--- src/pagas/users.vue
+++ src/pagas/users.vue
@@ -21,13 +21,19 @@
         <el-table-column prop="role_name" label="角色" ></el-table-column>
         <el-table-column prop="sign" label="状态" align="center"><template slot-scope="scope"><el-switch v-model="scope.row.mg_state" active-color="#13ce66" inactive-color="#ff4949" @change="changeState(scope.row)"></el-switch></template></el-table-column>
         <el-table-column prop="setting" label="操作" width="250" align="center">
-            <el-button type="primary" icon="el-icon-edit"></el-button>
-            <el-button type="warning" icon="el-icon-delete"></el-button>
-            <el-tooltip  effect="dark" content="分配角色" placement="top">
-                <el-button type="primary" icon="el-icon-aim"></el-button>
+          <!--          修改信息-->
+          <template slot-scope="scope">
+            <el-button type="primary" icon="el-icon-edit" @click="showEditDialog(scope.row.id)"/>
+            <!--          删除用户-->
+            <el-button type="warning" icon="el-icon-delete" @click="deleteUser(scope.row)"/>
+            <!--          分配权限-->
+            <el-tooltip effect="dark" content="分配角色" placement="top">
+              <el-button type="primary" icon="el-icon-aim"/>
             </el-tooltip>
+          </template>
         </el-table-column>
     </el-table>
+  <!--  底部跳转页面-->
     <div class="block">
         <span class="demonstration"></span>
         <el-pagination
@@ -41,27 +47,57 @@
         </el-pagination>
     </div>
 </el-card>
+  <!--  添加用户-->
     <el-dialog title="添加用户" :visible.sync="dialogVisible" width="40%" before-close="true">
-        <el-form :model="Form" :rules="rules" ref="FormRef" label-width="150px" class="FormRef">
-            <el-form-item label="姓 名:" prop="username" ><el-input type="text" v-model="Form.username" style="width: 80%"></el-input></el-form-item>
-            <el-form-item label="邮 箱:" prop="email"><el-input type="email" v-model="Form.email" style="width: 46.5%"></el-input><el-select v-model="Tem_email" placeholder="请选择">
-                <el-option v-for="item in select_options" :key="item.value" :label="item.label" :value="item.value"></el-option>
-            </el-select></el-form-item>
-            <el-form-item label="电 话:" prop="mobile"><el-input type="tel" v-model="Form.mobile" style="width: 80%"></el-input></el-form-item>
-            <el-form-item label="密 码:" prop="text_password"><el-input type="password" show-password v-model="test_password" style="width: 80%"></el-input></el-form-item>
-            <el-form-item label="确认密码:" prop="password"><el-input type="password" show-password v-model="Form.password" style="width: 80%"></el-input></el-form-item>
-        </el-form>
-  <span slot="footer" class="dialog-footer">
-    <el-button @click="addUserCancel('FormRef')">取 消</el-button>
-    <el-button type="primary" @click="addUserConfirm()">确 定</el-button>
-  </span>
+      <el-form :model="Form" :rules="rules" ref="FormRef" label-width="150px" class="FormRef">
+        <el-form-item label="姓 名:" prop="username">
+          <el-input type="text" v-model="Form.username" style="width: 80%"/>
+        </el-form-item>
+        <el-form-item label="邮 箱:" prop="email" size="medium">
+          <el-input type="email" v-model="Form.email" style="width: 39%"/>
+          <el-select v-model="Tem_email" placeholder="请选择" style="margin-left: 20px">
+            <el-option v-for="item in select_options" :key="item.value" :label="item.label" :value="item.value"/>
+          </el-select>
+        </el-form-item>
+        <el-form-item label="电 话:" prop="mobile">
+          <el-input type="tel" v-model="Form.mobile" style="width: 80%"/>
+        </el-form-item>
+        <el-form-item label="密 码:" prop="text_password">
+          <el-input type="password" show-password v-model="test_pass" style="width: 80%"/>
+        </el-form-item>
+        <el-form-item label="确认密码:" prop="password">
+          <el-input type="password" show-password v-model="Form.password" style="width: 80%"/>
+        </el-form-item>
+      </el-form>
+      <span slot="footer" class="dialog-footer">
+          <el-button @click="addUserCancel('FormRef')">取 消</el-button>
+          <el-button type="primary" @click="addUserConfirm()">确 定</el-button>
+        </span>
     </el-dialog>
+  <!--  编辑用户页面-->
+  <el-dialog title="编辑信息" :visible="EditdialogVisible" width="40%" before-close="true">
+    <el-form :model="EditForm" :rules="rules" label-width="150px" label-position="center" ref="editFormRef">
+      <el-form-item label="用户名 : " prop="username">
+        <el-input v-model="EditForm.username" :disabled="true" style="width: 80%"/>
+      </el-form-item>
+      <el-form-item label="邮箱地址 : " prop="email">
+        <el-input v-model="EditForm.email" style="width: 80%"/>
+      </el-form-item>
+      <el-form-item label="联系方式 : " prop="mobile">
+        <el-input v-model="EditForm.mobile" style="width: 80%"/>
+      </el-form-item>
+    </el-form>
+    <span slot="footer" class="dialog-footer">
+      <el-button @click="editUserCancel()">取 消</el-button>
+      <el-button type="primary" @click="editUserConfirm()">确 定</el-button>
+    </span>
+  </el-dialog>
 </div>
 </template>
 <script>
 export default {
   data () {
-    const emailRex = /^[a-zA-Z0-9_]/
+    const emailRex = /^[a-zA-Z0-9_]+[a-zA-Z0-9_]/
     const telRex = /^1+[3-9]+[0-9]{9}/
     const emailTest = (rule, value, callback) => {
       if (!value) {
@@ -81,11 +117,17 @@ export default {
       }
       callback()
     }
+    const passwordTest = (rule, value, callback) => {
+      if (!this.test_pass) {
+        return callback(new Error('请输入密码'))
+      }
+      callback()
+    }
     const passTest = (rule, value, callback) => {
       if (!value) {
         return callback(new Error('请再次输入密码'))
       }
-      if (value !== this.test_password) {
+      if (value !== this.test_pass) {
         return callback(new Error('两次密码不一致'))
       }
       callback()
@@ -94,42 +136,75 @@ export default {
       page: {
         query: '',
         pagenum: 1,
-        pagesize: 1
+        pagesize: 10
       },
-      Form: {
+      EditForm: {}, // 修改用户信息,
+      Form: { // 增加用户
         username: '',
         password: '',
         email: '',
         mobile: ''
       },
-      Tem_email: '@163.com',
-      rules: {
+      Tem_email: '@163.com', // 临时变量
+      rules: { // 增加用户验证规则
         username: [
-          { required: true, message: '请输入名称', trigger: 'blur' },
-          { min: 3, max: 15, message: '长度在 3 到 15 个字符', trigger: 'blur' }
+          {
+            required: true,
+            message: '请输入名称',
+            trigger: 'blur'
+          },
+          {
+            min: 3,
+            max: 15,
+            message: '长度在 3 到 15 个字符',
+            trigger: 'blur'
+          }
         ],
         email: [
-          { validator: emailTest, required: true, trigger: 'blur' }
+          {
+            validator: emailTest,
+            required: true,
+            trigger: 'blur'
+          }
         ],
         mobile: [
-          { required: true, validator: telTest, trigger: 'blur' }
+          {
+            required: true,
+            validator: telTest,
+            trigger: 'blur'
+          }
         ],
         password: [
-          { required: true, validator: passTest, trigger: 'blur' }
+          {
+            required: true,
+            validator: passTest,
+            trigger: 'blur'
+          }
         ],
         text_password: [
-          { required: true, message: '请输入密码', trigger: 'blur' }
+          {
+            required: true,
+            validator: passwordTest,
+            trigger: 'blur'
+          }
         ]
       },
-      test_password: '',
+      test_pass: '',
       dialogVisible: false,
+      EditdialogVisible: false, // 修改用户信息
       loading: true,
       total: 0,
       input: '',
       tableData: [{}],
       select_options: [
-        { value: '@163.com', label: '@163.com' },
-        { value: '@qq.com', label: '@qq.com' }
+        {
+          value: '@163.com',
+          label: '@163.com'
+        },
+        {
+          value: '@qq.com',
+          label: '@qq.com'
+        }
       ]
     }
   },
@@ -137,7 +212,7 @@ export default {
     this.getUserList()
   },
   methods: {
-    getUserList () {
+    getUserList () { // 调用API获取用户信息
       this.$http.get('users', { params: this.page }).then(res => {
         this.tableData = res.data.data.users
         this.total = res.data.data.total
@@ -146,7 +221,7 @@ export default {
       })
       this.loading = false
     },
-    handleSizeChange (val) {
+    handleSizeChange (val) { // 改变页面数量
       this.page.pagesize = val
       this.getUserList()
     },
@@ -157,18 +232,18 @@ export default {
     addUser () {
       this.dialogVisible = true
     },
-    changeState (e) {
+    changeState (e) { // 改变状态
       this.$http.put(`users/${e.id}/state/${e.mg_state}`)
     },
     reback () {
       this.page.query = ''
       this.getUserList()
     },
-    addUserCancel (e) {
+    addUserCancel (e) { // 取消操作
       this.dialogVisible = false
       this.$refs[e].resetFields()
     },
-    addUserConfirm () {
+    addUserConfirm () { // 增加用户
       this.$refs.FormRef.validate(valid => {
         if (!valid) {
           this.$message.error('请填写信息')
@@ -183,9 +258,58 @@ export default {
             if (res.data.meta.status === 201) {
               this.$message.success(res.data.meta.msg)
               this.dialogVisible = false
+              this.getUserList()
             }
           })
         }
+      })
+    },
+    async showEditDialog (e) { // 编辑用户信息
+      this.EditdialogVisible = true
+      const { data: res } = await this.$http.get('users/' + e)
+      if (res.meta.status !== 200) {
+        return this.$message.error('查询信息失败')
+      }
+      this.EditForm = res.data
+    },
+    editUserCancel () { //
+      this.EditdialogVisible = false
+    },
+    editUserConfirm () {
+      this.$refs.editFormRef.validate(async valid => {
+        if (valid) {
+          const { data: res } = await this.$http.put('users/' + this.EditForm.id, {
+            email: this.EditForm.email,
+            mobile: this.EditForm.mobile
+          })
+          console.log(res)
+          if (res.meta.status === 200) {
+            this.$message.success('更新成功!')
+            this.EditdialogVisible = false
+            this.getUserList()
+          } else {
+            return this.$message.error('编辑用户信息失败!')
+          }
+        } else {
+          return this.$message.error('请输入正确格式!')
+        }
+      })
+    },
+    deleteUser (e) {
+      this.$confirm(`即将删除用户:${e.username}，是否继续?`, '警告', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      }).then(() => {
+        const { data: res } = this.$http.delete('users/' + e.id)
+        if (res.meta.status === 200) {
+          this.$message.success('删除成功')
+          this.getUserList()
+        } else {
+          return this.$message.error('删除失败')
+        }
+      }).catch(() => {
+        this.$message.info('取消操作')
       })
     }
   }
