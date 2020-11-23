d81577bf687b99a2ee3f9b21896d8655e90be114
Jeff-wu-hub <15809812381@163.com>
2020-11-23 19:32:19 +0800

    上传订单管理
    Signed-off-by: Jeff-wu-hub <15809812381@163.com>

--- src/pagas/orders.vue
+++ src/pagas/orders.vue
@@ -1,5 +1,17 @@
 <template>
-    <div><h3>订单管理</h3></div>
+  <div>
+    <el-breadcrumb separator-class="el-icon-arrow-right" style="margin-bottom: 20px">
+      <el-breadcrumb-item to="/">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>订单管理</el-breadcrumb-item>
+      <el-breadcrumb-item>订单列表</el-breadcrumb-item>
+    </el-breadcrumb>
+    <el-card>
+      <el-input style="width: 30%;margin:0 20px 20px 0" placeholder="请输入查询内容" size="small"/>
+      <el-button type="primary" size="small">查询</el-button>
+      <el-table border stripe size="small" highlight-current-row>
+      </el-table>
+    </el-card>
+  </div>
 </template>
 <script></script>
 <style></style>
