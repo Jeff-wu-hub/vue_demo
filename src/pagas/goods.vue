1c01ac50ad0c8f65626de3cd2e713225c62a321b
Jeff-wu-hub <15809812381@163.com>
2020-11-26 22:16:55 +0800

    标签功能失效
    Signed-off-by: Jeff-wu-hub <15809812381@163.com>

--- src/pagas/goods.vue
+++ src/pagas/goods.vue
@@ -1,19 +1,94 @@
 <template>
     <div>
-<!--      面包屑-->
+      <!--      面包屑-->
       <el-breadcrumb separator-class="el-icon-arrow-right" style="margin-bottom: 20px;">
         <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
         <el-breadcrumb-item>商品管理</el-breadcrumb-item>
         <el-breadcrumb-item>商品列表</el-breadcrumb-item>
       </el-breadcrumb>
-<!--      添加商品选项-->
-      <el-card v-if="addGoods">
-        123
+      <!--      添加商品选项-->
+      <el-card v-if="addGoods" style="width: 80%;margin-left: 50%;transform: translate(-50%)">
+        <el-tag style="width: 100%;text-align: center" type="info" class="el-icon-info">添加商品</el-tag>
+        <el-steps :active="active" finish-status="success"
+                  style="margin-left: 50%;transform: translate(-50%);width: 80%">
+          <el-step title="基本信息"/>
+          <el-step title="商品参数"/>
+          <el-step title="商品属性"/>
+          <el-step title="商品图片"/>
+          <el-step title="商品内容"/>
+        </el-steps>
+        <el-form :model="addGoodsForm" :rules="rules" ref="addGoodsForm" label-width="100px" class="demo-ruleForm"
+                 label-position="top">
+          <el-tabs tab-position="left" style="margin-top: 20px" v-model="active" :before-leave="leave"
+                   @tab-click="click">
+            <el-tab-pane label="基本信息">
+              <el-form-item label="商品名称 ： " label-width="100px" style="margin-left: 50%;transform: translate(-50%)"
+                            prop="goods_name">
+                <el-input v-model="addGoodsForm.goods_name" style="width: 80%"/>
+              </el-form-item>
+              <el-form-item label="商品价格 ： " label-width="100px" style="margin-left: 50%;transform: translate(-50%)"
+                            prop="goods_price">
+                <el-input v-model="addGoodsForm.goods_price" style="width: 80%"/>
+              </el-form-item>
+              <el-form-item label="商品数量 ： " label-width="100px" style="margin-left: 50%;transform: translate(-50%)"
+                            prop="goods_number">
+                <el-input v-model="addGoodsForm.goods_number" style="width: 80%"/>
+              </el-form-item>
+              <el-form-item label="商品重量 ： " label-width="100px" style="margin-left: 50%;transform: translate(-50%)"
+                            prop="goods_weight">
+                <el-input v-model="addGoodsForm.goods_weight" style="width: 80%"/>
+              </el-form-item>
+              <el-form-item label="分 类 ： " label-width="100px" style="margin-left: 50%;transform: translate(-50%)"
+                            prop="goods_cat">
+                <el-cascader v-model="addGoodsForm.goods_cat" :options="options" :props="props"
+                             style="width: 80%"></el-cascader>
+              </el-form-item>
+            </el-tab-pane>
+            <el-tab-pane label="商品参数" name="1" @click="getcategories">
+              <el-form-item :label="item.attr_name" v-for="item in manyTableData" :key="item.attr_id">
+                <el-checkbox-group v-model="item.attr_vals">
+                  <el-checkbox v-for="(cb, i) in item.attr_vals" :label="cb" :key="i" border/>
+                </el-checkbox-group>
+              </el-form-item>
+            </el-tab-pane>
+            <el-tab-pane label="商品属性" name="2">
+              <el-form-item :label="item.attr_name" v-for="item in manyTableData1" :key="item.attr_id">
+                <el-input v-model="item.attr_vals" style="width: 60%;margin-left: 50%;transform: translate(-50%)"/>
+              </el-form-item>
+            </el-tab-pane>
+            <el-tab-pane label="商品图片" name="3">
+              <el-form-item>
+                <el-upload
+                  class="upload-demo"
+                  action="http://127.0.0.1:8888/api/private/v1/upload"
+                  :headers="headerObj"
+                  :on-preview="handlePreview"
+                  :on-success="upSuccess"
+                  list-type="picture">
+                  <el-button size="small" type="primary">点击上传</el-button>
+                  <div slot="tip" class="el-upload__tip">只能上传jpg/png文件，且不超过500kb</div>
+                </el-upload>
+              </el-form-item>
+            </el-tab-pane>
+            <el-tab-pane label="商品内容" name="4">
+              <quill-editor class="editor"
+                            ref="myTextEditor"
+                            v-model="addGoodsForm.goods_introduce"
+                            :options="editorOption"
+                            @blur="onEditorBlur($event)"
+                            @focus="onEditorFocus($event)"
+                            @ready="onEditorReady($event)"
+                            @change="onEditorChange($event)">
+              </quill-editor>
+              <el-button type="danger" @click="addGoodsInfo">点击添加商品</el-button>
+            </el-tab-pane>
+          </el-tabs>
+        </el-form>
       </el-card>
       <el-card v-else>
-<!--        添加商品按钮-->
+        <!--        添加商品按钮-->
         <el-button type="primary" style="margin:0 0 10px 10px" @click="addGoods=true">添加商品</el-button>
-<!--        表格开始-->
+        <!--        表格开始-->
         <el-table
           :data="goodsList"
           style="width: 100%;margin-bottom: 20px"
@@ -70,24 +145,81 @@
           <el-button type="primary" @click="editConfirm" size="small">确 定</el-button>
         </span>
       </el-dialog>
+      <el-dialog :visible.sync="photo">
+        <h1 style="margin: 0 30% 10% 0">图片预览</h1>
+        <el-image :src="photoSrc" style="width: 20vw;margin-left: 50%;transform: translate(-50%)"/>
+      </el-dialog>
     </div>
 </template>
 <script>
 export default {
   data () {
     return {
+      photoSrc: '',
+      photo: false,
+      rules: {
+        goods_name: [{
+          required: true,
+          message: '请输入商品名称',
+          trigger: 'blur'
+        }],
+        goods_price: [{
+          required: true,
+          message: '请输入商品价格',
+          trigger: 'blur'
+        }],
+        goods_number: [{
+          required: true,
+          message: '请输入商品数量',
+          trigger: 'blur'
+        }],
+        goods_weight: [{
+          required: true,
+          message: '请输入商品重量',
+          trigger: 'blur'
+        }],
+        goods_cat: [{
+          required: true,
+          message: '请选择商品分类',
+          trigger: 'blur'
+        }]
+      },
+      headerObj: {
+        Authorization: window.sessionStorage.getItem('token')
+      },
+      options: [],
+      addGoodsForm: {
+        attrs: []
+      },
+      active: 0,
       goodsRow: [], // 要修改的内容，编辑框
       editDialog: false, // 编辑框弹出窗口
-      addGoods: false, // 显示添加商品标识
+      addGoods: true, // 显示添加商品标识
       goodsList: [], // 商品列表
       page: { // 页面商品页码及显示数量
         pagenum: 1, // 当前页面
         pagesize: 15 // 当前页面多少个内容
-      }
+      },
+      props: {
+        expandTrigger: 'hover',
+        children: 'children',
+        label: 'cat_name',
+        value: 'cat_id'
+      },
+      manyTableData: [],
+      manyTableData1: []
     }
   },
   methods: {
     // 获取商品列表方法
+    leave (activeName, oldactiveName) {
+      activeName = Number(activeName)
+      oldactiveName = Number(oldactiveName)
+      if (activeName > (oldactiveName + 1)) {
+        this.$message.info('不要跨越步骤')
+        return false
+      }
+    },
     async getgoodsList () {
       const { data: res } = await this.$http.get('goods', {
         params: this.page
@@ -112,17 +244,91 @@ export default {
     },
     // 编辑提交确认按钮
     async editConfirm () {
-      console.log(this.goodsRow)
-      const { data: res } = this.$http.post(`goods/${this.goodsRow.goods_id}`, this.goodsRow)
-      console.log(res)
+      await this.$http.post(`goods/${this.goodsRow.goods_id}`, this.goodsRow)
+    },
+    async getOption () {
+      const { data: res } = await this.$http.get('categories', {
+        params: {
+          type: 3
+        }
+      })
+      this.options = res.data
+    },
+    click (e) {
+      if (e.index === '1') {
+        this.getcategories('many')
+      }
+      if (e.index === '2') {
+        this.getcategories1('only')
+      }
+    },
+    async getcategories (e) {
+      const { data: res } = await this.$http.get(`categories/${this.addGoodsForm.goods_cat[2]}/attributes`, {
+        params: {
+          sel: e
+        }
+      })
+      res.data.forEach(item => {
+        item.attr_vals =
+          item.attr_vals.length === 0 ? [] : item.attr_vals.split(' ')
+      })
+      this.manyTableData = res.data
+    },
+    async getcategories1 (e) {
+      const { data: res } = await this.$http.get(`categories/${this.addGoodsForm.goods_cat[2]}/attributes`, {
+        params: {
+          sel: e
+        }
+      })
+      this.manyTableData1 = res.data
+    },
+    handlePreview (file) {
+      this.photo = true
+      this.photoSrc = file.response.data.url
+    },
+    addGoodsInfo () {
+      this.addGoodsForm.goods_cat = this.addGoodsForm.goods_cat.join(',')
+      this.manyTableData.forEach(item => {
+        const obj = {
+          attr_id: item.attr_id,
+          attr_value: item.attr_vals.join(',')
+        }
+        this.addGoodsForm.attrs.push(obj)
+      })
+      this.$http.post('goods', this.addGoodsForm)
+      console.log(this.addGoodsForm)
+    },
+    upSuccess (response) {
+      this.addGoodsForm.pics = response.data.tmp_path
     }
   },
   mounted () {
     this.getgoodsList() // 获取商品信息
+    this.getOption()
   },
   computed: {
+    editor () {
+      return this.$refs.myTextEditor.quillEditor
+    }
   }
 }
 </script>
 <style>
+  .el-checkbox {
+    margin: 20px 10px 0 0 !important;
+  }
+
+  .el-form-item__label {
+    font-size: 10px !important;
+    width: 150px !important;
+  }
+
+  .ql-container {
+    height: 300px !important;
+  }
+
+  .editor {
+    line-height: normal !important;
+    height: 400px;
+  }
 </style>
--- src/pagas/params.vue
+++ src/pagas/params.vue
@@ -1,5 +1,125 @@
 <template>
-    <div><h3>分类参数</h3></div>
+  <div>
+    <el-breadcrumb separator-class="el-icon-arrow-right" style="margin-bottom: 20px">
+      <el-breadcrumb-item to="/">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>商品管理</el-breadcrumb-item>
+      <el-breadcrumb-item>分类参数</el-breadcrumb-item>
+    </el-breadcrumb>
+    <el-card>
+      <el-tag type="danger" style="width: 100%">
+        <el-icon class="el-icon-info"/>
+        注意：只允许为第三集分类设置相关参数
+      </el-tag>
+      <el-form>
+        <el-form-item label="选择商品分类 ： " label-width="100px">
+          <el-cascader v-model="value" :options="options" :props="expand" @change="handleChange" style="width: 20%"
+                       placeholder="请选择商品"/>
+        </el-form-item>
+      </el-form>
+      <el-tabs v-model="active" @tab-click="handleClick">
+        <el-tab-pane label="动态参数" name='1'>
+          <el-button type="primary" size="small" style="margin: 10px 0 20px 0">添加参数</el-button>
+          <el-table :data="manyTableData" size="small" border stripe fit highlight-current-row>
+            <el-table-column type="expand" prop="attr_name">
+              <template slot-scope="scops">
+                <el-tag v-for="(item, id) in scops.row.attr_vals" :key="id" closable disable-transitions hit>{{item}}
+                </el-tag>
+              </template>
+            </el-table-column>
+            <el-table-column type="index" index label="序号" width="50px" align="center"/>
+            <el-table-column prop="attr_name" align="center"/>
+            <el-table-column label="操作" align="center">
+              <template slot-scope="scops">
+                <el-button type="primary" @click="edit = true" style="margin-right: 20px" size="mini">修改</el-button>
+                <el-button type="danger" @click="delete(scops.row)" size="mini">删除</el-button>
+              </template>
+            </el-table-column>
+          </el-table>
+        </el-tab-pane>
+        <el-tab-pane label="静态参数" name="2">
+          <el-button type="primary" size="small" style="margin: 10px 0 20px 0">添加参数</el-button>
+          <el-table :data="manyTableData" size="small" border stripe fit highlight-current-row>
+            <el-table-column type="expand" prop="attr_name">
+              <template slot-scope="scops">
+                <el-tag v-for="(item, id) in scops.row.attr_vals" :key="id" closable disable-transitions hit
+                        style="width: auto">{{item}}
+                </el-tag>
+                <el-input class="input-new-tag" v-if="inputVisible" v-model="inputValue" ref="saveTagInput" size="small"
+                          @keyup.enter.native="handleInputConfirm" @blur="handleInputConfirm"/>
+                <el-button v-else size="small" @click="showInput">+ New Tag</el-button>
+              </template>
+            </el-table-column>
+            <el-table-column type="index" index label="序号" width="50px" align="center"/>
+            <el-table-column prop="attr_name" align="center"/>
+            <el-table-column label="操作" align="center">
+              <template slot-scope="scops">
+                <el-button type="primary" @click="edit = true" style="margin-right: 20px" size="mini">修改</el-button>
+                <el-button type="danger" @click="delete(scops.row)" size="mini">删除</el-button>
+              </template>
+            </el-table-column>
+          </el-table>
+        </el-tab-pane>
+      </el-tabs>
+    </el-card>
+    <el-dialog :visible.sync="edit"></el-dialog>
+  </div>
 </template>
-<script></script>
+<script>
+  export default {
+    data () {
+      return {
+        inputValue: '',
+        inputVisible: false,
+        sel: 'many',
+        manyTableData: [],
+        edit: false,
+        active: '1',
+        value: [],
+        options: [],
+        expand: {
+          expandTrigger: 'hover',
+          label: 'cat_name',
+          value: 'cat_id'
+        }
+      }
+    },
+    methods: {
+      delete (e) {
+        console.log(e)
+      },
+      async handleChange () {
+        const { data: res } = await this.$http.get(`categories/${this.value[2]}/attributes`, {
+          params: {
+            sel: this.sel
+          }
+        })
+        this.manyTableData = res.data
+        this.manyTableData.forEach(item => {
+          item.attr_vals = item.attr_vals ? item.attr_vals = item.attr_vals.split(',') : []
+        })
+        console.log(this.manyTableData)
+      },
+      handleClick (e) {
+        if (e.name === '2') {
+          this.sel = 'only'
+          this.handleChange()
+        } else {
+          this.sel = 'many'
+          this.handleChange()
+        }
+      },
+      async getInfo () {
+        const { data: res } = await this.$http.get('categories', {
+          params: {
+            type: 3
+          }
+        })
+        this.options = res.data
+      }
+    },
+    mounted () {
+      this.getInfo()
+    }
+  }
+</script>
 <style></style>
